4d241f3c238bff32746c0f54f783eb9fdd6aa5ae
Jeff Fredericks <jeffwfredericks@gmail.com>
2017-03-10 11:55:32 -0700

    Commented the callback assignement.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
+// object literal with two methods: add and multiply
 export default {
+  //takes two numbers and returns the result (as a callback) of adding the two numbers together
   add(num1 = 0, num2 = 0, callback) {
     return callback(null, num1 + num2);
   },
+  //takes tw numbers and returns the result (as a callback) of multiplying the two numbers together
   multiply(num1 = 0, num2 = 0, callback) {
     return callback(null, num1 * num2);
   }
